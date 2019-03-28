71c209ce261f986936ea1793fb32fe1d76a93c92
Brad Haas <bradh@presidio.com>
2019-03-28 09:05:24 -0400

    fix time offshift from cisco

--- psirt.py
+++ psirt.py
@@ -76,6 +76,7 @@ def construct_message_alert(advisory):
 def date_from_string(s):
     # convert string to datetime object
     d = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S%z")
+    d = d - timedelta(hours=3)
     return d.replace(tzinfo=None)
 
 
